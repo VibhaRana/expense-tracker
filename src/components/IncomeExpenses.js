5c569f606b4a514abe57241409be1e31e59e3270
Reet Arora <reet@Reets-MacBook-Pro.local>
2021-06-21 15:19:20 -0700

    Layout done and reusable components made

---
+++ src/components/IncomeExpenses.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function IncomeExpenses() {
+    return (
+        <div className="inc-exp-container">
+        <div>
+          <h4>Income</h4>
+  <p className="money plus">MOney</p>
+        </div>
+        <div>
+          <h4>Expense</h4>
+  <p className="money minus">Money</p>
+        </div>
+      </div>
+    )
+}
+
+export default IncomeExpenses
